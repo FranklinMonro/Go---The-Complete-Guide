499558fa2dd652cc39cb5e352ab615bbe5903f7a
FranklinMonro <fbg2468@gmail.com>
2025-12-03 13:15:27 +0000

    print value

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Investment Calculator")
+	fmt.Println("Investment Calculator")
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 
-	var futureValue = float64(investmentAmount) * (1 + expectedReturnRate/100)
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Println("Future Value:", futureValue)
 }
